414c706f82e1ea2cb6bde15eee9b45c8847e7078
TarcoRodrigo <tarcorodrigo@gmail.com>
2026-04-12 02:42:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'financex-v202604120534';
+var CACHE = 'financex-v202604120541';
 var ARQUIVOS = ['./index.html', './style.css', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
